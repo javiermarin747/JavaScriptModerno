30b38849fc144d29eccba40ae1ff46ee3e6802de
Javier Marin Corcoles <javiermarin747@gmail.com>
2023-12-10 18:42:41 +0100

    Incio2

---
+++ 01_fundamentos/assets/js/alerts.js
@@ -0,0 +1,8 @@
+alert('Hola Javi');
+
+let nombre=prompt('¿como te llamas ?','Introduce tu monbre');
+
+console.log(nombre);
+
+const seleccion=confirm('Estas seguro ???');
+console.log(seleccion);
---
+++ 01_fundamentos/assets/js/app.js
@@ -0,0 +1,33 @@
+// console.log('Hola js');
+
+// comentar un alinea entera crl+k y ctrl+c
+// descomentar ctl+k ctrl+u 
+
+// var -> inicializacion antigua... no utilizar....mejor let
+
+let a=10;
+let b=50;
+let c="hola";
+let d="Adios";
+const saludo=c+d;
+//var b=10;
+// const c=10;
+
+// let x="Hola ", z="Adios";
+// let j=20,
+    // f=2,
+    // s="gg";
+
+// console.log(x+z);
+
+// console.error('a',a);
+// console.warn('b',b);
+// console.info('a+b',a+b);
+
+ console.log('%c Mis Variables','color:green');
+ console.log({a});
+ console.log({b});
+ console.table({a,b,saludo});
+ console.log({saludo});
+
+var miNonbre="Javi";
