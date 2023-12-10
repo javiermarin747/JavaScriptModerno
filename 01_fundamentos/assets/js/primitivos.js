ad8c655c911844a76aa5f68065438a6c4c5099e3
Javier Marin Corcoles <javiermarin747@gmail.com>
2023-12-10 18:58:50 +0100

    Primitivos

---
+++ 01_fundamentos/assets/js/primitivos.js
@@ -0,0 +1,25 @@
+
+
+let nombre='Javier Marin';
+console.log(nombre);
+
+nombre=`Javi backtip`;
+console.log(typeof nombre);
+
+let esMarvel = true;
+console.log( typeof esMarvel );
+
+let edad = 33;
+console.log(typeof edad);
+
+let superPoder ;
+console.log( typeof superPoder );
+
+let soyNull = null ;
+console.log( typeof soyNull );
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log( typeof symbol1 );
+
+console.log( symbol1 === symbol2 );
